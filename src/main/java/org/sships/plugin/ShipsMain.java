f66a6a26574663d07488bfb198618f6e2c09b570
mosemister <nekronmoseley@gmail.com>
2019-04-17 16:28:28 +0100

    Cleaned up code 3

--- src/main/java/org/sships/plugin/ShipsMain.java
+++ src/main/java/org/sships/plugin/ShipsMain.java
@@ -6,17 +6,18 @@ import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.event.game.state.GameStartingServerEvent;
 import org.spongepowered.api.plugin.Plugin;
 
-@Plugin(id = ShipsMain.PLUGIN_ID, name = ShipsMain.PLUGIN_NAME, version = ShipsMain.PLUGIN_VERSION)
+@Plugin(id = ShipsMain.PLUGIN_ID, name = ShipsMain.PLUGIN_NAME, description = ShipsMain.PLUGIN_DESCRIPTION, version = ShipsMain.PLUGIN_VERSION)
 public class ShipsMain {
 
     public static final String PLUGIN_ID = "ships";
     public static final String PLUGIN_NAME = "Ships";
+    public static final String PLUGIN_DESCRIPTION = "Move structures with a single click";
     public static final String PLUGIN_VERSION = "6.0.0.0";
 
     private static ShipsMain plugin;
 
     public ShipsMain(){
-        this.plugin = this;
+        plugin = this;
     }
 
     @Listener
