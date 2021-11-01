9d699cc0248fa9f8e834ec4c8906b0d3d0b05e38
mosemister <nekronmoseley@gmail.com>
2021-11-01 13:57:25 +0000

    More cleanup

--- src/main/java/org/sships/plugin/cmd/ShipsTestCommand.java
+++ src/main/java/org/sships/plugin/cmd/ShipsTestCommand.java
@@ -3,7 +3,6 @@ package org.sships.plugin.cmd;
 import org.spongepowered.api.command.Command;
 import org.spongepowered.api.command.CommandExecutor;
 import org.spongepowered.api.command.CommandResult;
-import org.spongepowered.api.command.exception.CommandException;
 import org.spongepowered.api.command.parameter.CommandContext;
 import org.spongepowered.api.registry.RegistryTypes;
 
@@ -16,15 +15,16 @@ public class ShipsTestCommand {
     public static class CreateBlockTypeList implements CommandExecutor {
 
         @Override
-        public CommandResult execute(CommandContext context) throws CommandException {
-            File file = new File("ShipsTest/Items.txt");
+        public CommandResult execute(CommandContext context) {
+            File file = new File("ShipsTest" + File.pathSeparatorChar + "Items.txt");
+
             file.getParentFile().mkdirs();
             try {
                 file.createNewFile();
                 FileWriter writer = new FileWriter(file);
                 RegistryTypes.ITEM_TYPE.get().stream().forEach(it -> {
                     try {
-                        writer.write("public static final Optional<BlockType> " + it.key(RegistryTypes.ITEM_TYPE).value().toUpperCase() + " = CorePlugin.getPlatform().get(new ItemTypes1V12(\"" + it.key(RegistryTypes.ITEM_TYPE).asString() + "\"));\n");
+                        writer.write("public static final Optional<BlockType> " + it.key(RegistryTypes.ITEM_TYPE).value().toUpperCase() + " = CorePlugin.getPlatform().get(new ItemTypes1V12(\"" + it.key(RegistryTypes.ITEM_TYPE).asString() + "\"));" + File.separator);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
